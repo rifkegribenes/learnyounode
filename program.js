d104638bcb321df81fa4dbe98da043db8a87b1f8
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-13 07:27:23 +0000

    exercise 13

--- program.js
+++ program.js
@@ -1,15 +1,36 @@
 const http = require("http");
-const map = require("through2-map");
+const url = require("url");
 
-const file = process.argv[3];
 const port = process.argv[2];
 
+const unixtime = (time) => {
+ return { unixtime: time.getTime() };
+}
+
+const parsetime = (time) => {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds()
+  };
+}
+
 http.createServer((req, res) => {
-  if (req.method !== 'POST') {
-    return res.end('that wasn\'t a POST\n');
-  }
-  
-  req.pipe(map(chunk => 
-    chunk.toString().toUpperCase()
-  )).pipe(res)
-}).listen(port);
+  const urlObject = url.parse(req.url, true);
+  const pathname = urlObject.pathname;
+  const time = new Date(urlObject.query.iso);
+  let result;
+
+    if (pathname === '/api/unixtime') {
+      result = unixtime(time);
+    } else if (pathname === '/api/parsetime') {
+      result = parsetime(time);
+    }
+    if (result) {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(result));
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
+  }).listen(port);
