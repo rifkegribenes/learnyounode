342c18af0568ffc7ef643c5a4762d3a324b56ebf
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-13 06:53:33 +0000

    exercise 12

--- program.js
+++ program.js
@@ -1,10 +1,15 @@
 const http = require("http");
-const fs = require("fs");
+const map = require("through2-map");
 
 const file = process.argv[3];
 const port = process.argv[2];
 
 http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type' : 'text/plain' });
-  fs.createReadStream(file).pipe(res);
+  if (req.method !== 'POST') {
+    return res.end('that wasn\'t a POST\n');
+  }
+  
+  req.pipe(map(chunk => 
+    chunk.toString().toUpperCase()
+  )).pipe(res)
 }).listen(port);
